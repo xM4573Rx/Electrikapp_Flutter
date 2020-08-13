1d3d4840cddc488a6591119476d135f7a2f742c3
Rajual <julioalfonso8003@gmail.com>
2020-08-12 21:19:35 -0500

    Add List

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,4 @@
+import 'package:Electrikapp/class/graph.dart';
 import 'package:Electrikapp/services/auth.dart';
 import 'package:flutter/material.dart';
 
@@ -38,6 +39,90 @@ class _HomePageState extends State<HomePage> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton:
           FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
+      body: _body(),
     );
+  }
+
+  Widget _body() {
+    return SafeArea(
+        child: Column(
+      children: <Widget>[_selector(), _expenses(), _graph(), _list()],
+    ));
+  }
+
+  Widget _selector() => Container();
+  Widget _expenses() {
+    return Column(
+      children: <Widget>[
+        Text(
+          '\$2345.5',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40.0),
+        ),
+        Text(
+          'Total Ganancias',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+              color: Colors.blueGrey),
+        ),
+      ],
+    );
+  }
+
+  Widget _graph() {
+    return Container(
+      height: 250.0,
+      child: GraphWidget(),
+    );
+  }
+
+  Widget _item(IconData icon, String name, int porcent, double value) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: ListTile(
+        leading: Icon(
+          icon,
+          size: 32.0,
+        ),
+        title: Text(
+          name,
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+        subtitle: Text(
+          '$porcent% of expenses',
+          style: TextStyle(fontSize: 16.0, color: Colors.blueGrey),
+        ),
+        trailing: Container(
+          decoration: BoxDecoration(
+              color: Colors.blueAccent.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(5.0)),
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Text(
+              '\$$value',
+              style: TextStyle(color: Colors.blueAccent),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _list() {
+    return Expanded(
+        child: ListView.separated(
+      itemCount: 14,
+      itemBuilder: (context, index) => RaisedButton(
+        onPressed: () {},
+        child: _item(Icons.ac_unit, 'Agua', 20, 23),
+      )
+      /*_item(Icons.phone_android, 'Shopping', 14, 145.12)*/,
+      separatorBuilder: (context, index) {
+        return Container(
+          color: Colors.blueAccent.withOpacity(0.15),
+          height: 8,
+        );
+      },
+    ));
   }
 }
