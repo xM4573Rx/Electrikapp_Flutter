88cf6e0699539ef3c5c097871c1f4c0d43914b56
Rajual <julioalfonso8003@gmail.com>
2020-10-02 13:28:26 -0500

    Ajustes de Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,42 @@
 import 'package:Electrikapp/pages/home.dart';
 import 'package:Electrikapp/pages/login.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _loggedIn = false;
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'FetoCardia',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: HomePage()/*LoginPage(
+      debugShowCheckedModeBanner: false,
+      title: 'FetoCardia',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      /*home: HomePage()*/ /*LoginPage(
           title: 'Hola',
-        )*/);
+        ),*/
+      routes: {
+        '/': (BuildContext contex) {
+          if (_loggedIn) {
+            return HomePage();
+          } else {
+            return LoginPage(
+              onLoginSuccess: () {
+                setState(() {
+                  _loggedIn = true;
+                });
+              },
+            );
+          }
+        },
+      },
+    );
   }
 }
