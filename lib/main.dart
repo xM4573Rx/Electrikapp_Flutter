7a6c7810e032cf0114ed0fa616fa56d62b2e0076
Rajual <julioalfonso8003@gmail.com>
2020-10-02 13:29:03 -0500

    Ajuestes Login 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Electrikapp/models/login_state.dart';
 import 'package:Electrikapp/pages/home.dart';
 import 'package:Electrikapp/pages/login.dart';
 import 'package:flutter/material.dart';
@@ -5,38 +6,35 @@ import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _loggedIn = false;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'FetoCardia',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      /*home: HomePage()*/ /*LoginPage(
-          title: 'Hola',
-        ),*/
-      routes: {
-        '/': (BuildContext contex) {
-          if (_loggedIn) {
-            return HomePage();
-          } else {
-            return LoginPage(
-              onLoginSuccess: () {
-                setState(() {
-                  _loggedIn = true;
-                });
-              },
-            );
-          }
+    return ChangeNotifierProvider<LoginState>(
+      builder: (BuildContext context) => LoginState(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'FetoCardia',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        /*home: HomePage()*/ /*LoginPage(
+            title: 'Hola',
+          ),*/
+        routes: {
+          '/': (BuildContext contex) {
+            if (_loggedIn) {
+              return HomePage();
+            } else {
+              return LoginPage(
+                onLoginSuccess: () {
+                  setState(() {
+                    _loggedIn = true;
+                  });
+                },
+              );
+            }
+          },
         },
-      },
+      ),
     );
   }
 }
